e54687b4d4fa331a38b49f5a1b5af4563f69807d
Dhanraj <dhanraj.nilkanth@gmail.com>
2020-09-01 17:21:12 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,1 @@
+import 'package:flutter/material.dart';import 'RatingUI.dart';void main() => runApp(RatingAppUI());class RatingAppUI extends StatelessWidget {  @override  Widget build(BuildContext context) {    return MaterialApp(      debugShowCheckedModeBanner: false,      home: RatingUI(),    );  }}
