ab27694ce8f59d4304278c206019ce34981685eb
Dhanraj-MobileAppDev <52417093+DhanrajNilkanth@users.noreply.github.com>
2020-10-02 14:54:10 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,15 @@
-import 'package:flutter/material.dart';import 'RatingUI.dart';void main() => runApp(RatingAppUI());class RatingAppUI extends StatelessWidget {  @override  Widget build(BuildContext context) {    return MaterialApp(      debugShowCheckedModeBanner: false,      home: RatingUI(),    );  }}
+import 'package:flutter/material.dart';
+import 'RatingUI.dart';
+
+void main() => runApp(RatingAppUI());
+
+class RatingAppUI extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: RatingUI(),
+    );
+  }
+}
+
